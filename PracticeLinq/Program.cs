934de2820286946c7ebaf173af7180b54f68a4db
matt21ferg <matt21ferguson@gmail.com>
2022-01-28 13:24:11 -0600

    1st

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGameNames = new List<string>();
+            videoGameNames.Add("Nba2k");
+            videoGameNames.Add("Madden");
+            videoGameNames.Add("Nhl");
+            videoGameNames.Add("Mlb the show");
+            videoGameNames.Add("Call of Duty vangaurd");
+            var gamesOreded = (videoGameNames.OrderByDescending(x => x.Count()));
+            foreach (var item in gamesOreded)
+            {
+                Console.WriteLine(item);
+            }
+        } 
+    }
+}
